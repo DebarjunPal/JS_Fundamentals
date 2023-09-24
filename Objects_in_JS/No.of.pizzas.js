c9593c1f05fdc3a9799c6a094d88b1be83eaa7ba
Debarjun Pal <debarjunpal134@gmail.com>
2023-09-24 22:46:47 +0530

    Added TotalNo.OfPizzas Function

--- Objects_in_JS/No.of.pizzas.js
+++ Objects_in_JS/No.of.pizzas.js
@@ -2,4 +2,14 @@ function numberOfPizzas(order) {
     return order.pizzas;
 }
 
+module.exports = numberOfPizzas; 
+
+function numberOfPizzas(orders) {
+    let totalPizzas = 0
+    for(let i = 0; i < orders.length; i++){
+        totalPizzas += orders[i].pizzas; 
+    }
+    return totalPizzas;
+}
+
 module.exports = numberOfPizzas;
