df7f606f9b55bacc5c5421e8bef6fb487a5f5066
Debarjun Pal <debarjunpal134@gmail.com>
2023-11-13 22:00:47 +0530

    Added willEat function

---
+++ Logical_Operators/willEat.js
@@ -0,0 +1,10 @@
+function willEat(hasPizza, hasDonuts, hasCookies) {
+    if(hasPizza || hasDonuts || hasCookies){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+module.exports = willEat;
